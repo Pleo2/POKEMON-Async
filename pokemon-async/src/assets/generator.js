f7214eca1a4bee4078306ce734369d1c61aef734
pleo <leooel23m@gmail.com>
2022-11-28 14:33:38 -0400

    version mobile + feture add Favorites + hotFix

--- pokemon-async/src/assets/generator.js
+++ pokemon-async/src/assets/generator.js
@@ -1,53 +1,3 @@
-const APICard = "https://api.pokemontcg.io/v2/cards/";
-const optionsCard = {
-  method: "GET",
-  apiKey: "e45a77d6-5b1b-465d-a0ae-67038c51e6e8",
-};
-
-//--------------------------------------------------------//
-
-let cache;
-
-const fetchDataCard = async (urlApi, options) => {
-  const response = await fetch(urlApi, options);
-  const data = await response.json();
-
-  return data;
-};
-
-const getData = async () => {
-  const all = await fetchDataCard(APICard, optionsCard);
-  return all;
-};
-
-const getCache = () => {
-  (async () => {
-    try {
-      if (!localStorage.data) {
-        data = await getData();
-        let dataData = data.data;
-        const stringData = JSON.stringify(dataData);
-        localStorage.setItem("data", stringData);
-        // JSON.parse(localStorage.getItem("data"));
-        cache = JSON.parse(localStorage.data);
-        return
-
-      } else {
-        cache = JSON.parse(localStorage.data);
-         return cache
-
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  })();
-};
-
-getCache();
-
-const btn = null || document.querySelector("#btn");
-const specialCard = null || document.getElementById("Special-Card");
-const btn2 = null || document.querySelector("#btn2");
 
 const minNumber = 0;
 const maxNumber = 249;
@@ -72,3 +22,4 @@ const generateCard = () => {
 };
 
 btn.onclick = generateCard;
+
